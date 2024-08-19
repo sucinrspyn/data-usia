ea0fbfeeac15de7a327d85563254e48e8cf1ed75
suci <sopyansuci042@gmail.com>
2024-08-19 15:40:15 +0700

    first commit

---
+++ usia.php
@@ -0,0 +1,27 @@
+<?php
+
+$usia = 95;
+
+if ($usia > 90 || $usia > 90) {
+    echo "Usia 0-12 tahun : anak-anak";
+} else
+    echo "<br>";
+
+$usia = 85;
+echo "<br>";
+if ($usia > 80 || $usia > 80) {
+    echo "Usia 13-17 tahun : remaja";
+
+    $usia = 75;
+    echo "<br>";
+}
+if ($usia > 70 || $usia > 70) {
+    echo "Usia 18-59 tahun : dewasa";
+
+    $usia = 65;
+    echo "<br>";
+}
+if ($usia > 60 || $usia > 60) {
+    echo "Usia 60 tahun ke atas : lansia";
+} else {
+}
